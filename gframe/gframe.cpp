2066f03d04cabab88fc4ced43a1ff7e376325d83
jekabs.vidrusks <jekabs.vidrusks@IZV24-P009>
2025-03-25 11:39:21 +0200

    rufbof

---
+++ gframe/gframe.cpp
@@ -0,0 +1,23 @@
+
+
+
+struct GameData
+{
+	StateMachine Machine;
+	InputManager inputManager;
+	AssetManager AssetManager;
+	sf::RenderWindow window;
+	Tools tools;
+};
+
+
+
+class Gframe
+{
+public:
+	Gframe() {};
+	~Gframe() {};
+
+private:
+
+};
