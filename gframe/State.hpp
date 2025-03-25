4f6410572629504210ac8b56f40a32850f1b7476
jekabs.vidrusks <jekabs.vidrusks@IZV24-P009>
2025-03-25 12:15:02 +0200

    viss

--- gframe/State.hpp
+++ gframe/State.hpp
@@ -8,6 +8,7 @@ public:
 	virtual void HandleInput() = 0;
 	virtual void Update(float dt) = 0;
 	virtual void Draw(float dt) = 0;
+	
 
 	virtual void Pause() = 0;
 	virtual void Resume() = 0;
--- gframe/StateMachine.hpp
+++ gframe/StateMachine.hpp
@@ -8,12 +8,14 @@
 class StateMachine
 {
 public:
-	StateMachine() {};
+	StateMachine() : _isRemoving(0), _isAdding(0), _isreplacing(0) {};
 	~StateMachine() {};
 	void AddState(std::unique_ptr<State>  newState, bool isReplacing);
 	void RemoveState();
 	void ProcessStateChanges();
+	
 	std::unique_ptr<State>& GetActiveState();
+
 private:
 	std::stack<std::unique_ptr<State>> _states;
 	std::unique_ptr<State>  _newState;
