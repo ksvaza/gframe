e069d677d10c9597537225936601329a430097f3
ksvaza <ksvaza@gmail.com>
2025-03-26 11:45:15 +0200

    Revert "a"
    This reverts commit 4a8cf28382c4800edac550da99796f7187292ff9.
    
    # Conflicts:
    #	gframe/gframe.cpp - resolved

--- gframe/main.cpp
+++ gframe/main.cpp
@@ -1,20 +1,15 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "InputManager.hpp"
-#include "console.hpp"
 
 int main(void)
 {
     GLFWwindow* window;
 
-
     /* Initialize the library */
     if (!glfwInit())
         return -1;
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        return -1;
-    }
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
@@ -22,11 +17,7 @@ int main(void)
         glfwTerminate();
         return -1;
     }
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-		printf("Failed to initialize OpenGL context\n");
-        return -1;
-    }
+
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
