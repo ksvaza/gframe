4286586f0517c89490b9e327ddacfcbd5186af4d
jekabins <jekabins@DESKTOP-0VS77PC>
2025-04-26 16:01:24 +0300

    little texture testing and better get texture-unload texture

--- gframe/AssetManager.cpp
+++ gframe/AssetManager.cpp
@@ -1,7 +1,50 @@
 #include "AssetManager.hpp"
+#include "stb_image.h"
+#include <iostream>
 
-void AssetManager::LoadTexture(std::string name, std::string FileName)
+int8_t  AssetManager::LoadTexture(std::string_view name, std::string_view FileName)
 {
-	//int width = 0, height = 0, nrChannels = 0;
-	//_textures[name] = stbi_load(FileName.c_str(), &width, &height, &nrChannels, 0);
+	int Width, Height, nrChannels;
+	Texture texture;
+	texture.data.raw = stbi_load(FileName.data(), &Width, &Height, &nrChannels, 0);
+	if (!texture.data.raw)
+	{
+		std::cout << "Failed to load texture from:\n" << FileName << '\n';
+
+
+		//printf("Failed to load texture %s\n", FileName.data());
+		return -1;
+	}
+	texture.height = Height;
+	texture.width = Width;
+	texture.nrChannels = nrChannels;
+	_textures[name] = texture;
+	return 0;
+}
+Texture& AssetManager::GetTexture(std::string_view name)
+{
+	static Texture ErrorTexture; // kaut kads error texture ka piem. pink and black error texture
+	auto it = _textures.find(name);
+	if (it != _textures.end())
+	{
+		return it->second;
+	}
+	return ErrorTexture;
+}
+
+int8_t AssetManager::UnloadTexture(std::string_view name)
+{
+	auto it = _textures.find(name);
+	if (it != _textures.end())
+	{
+		stbi_image_free(it->second.data.raw);
+		_textures.erase(it);
+	}
+	return -1;
+}
+
+
+int8_t AssetManager::LoadFont(std::string_view name, std::string_view FileName)
+{
+	return 0;
 }
